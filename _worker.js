46f154a5b8a6d1a5fb7550098bd1b317fc6cea9a
rycoabbs <mniyuo8@gmail.com>
2025-09-05 19:34:33 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,8 +3,8 @@ import { connect } from "cloudflare:sockets";
 // import { Buffer } from "node:buffer";
 
 // Variables
-const rootDomain = "freecf2025.workers.dev"; // Ganti dengan domain utama kalian
-const serviceName = "nautica-prod"; // Ganti dengan nama workers kalian
+const rootDomain = "cfworker2.workers.devv"; // Ganti dengan domain utama kalian
+const serviceName = "cfkuryco"; // Ganti dengan nama workers kalian
 const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = ""; // Ganti dengan email yang kalian gunakan
 const accountID = ""; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
@@ -1712,4 +1712,5 @@ class Document {
 
     return this.html.replaceAll(/PLACEHOLDER_\w+/gim, "");
   }
+
 }
